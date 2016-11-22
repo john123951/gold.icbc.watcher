63136029ae29e7ed8c0b134725ccde1e42ee17ae
sweet <sweet123951@gmail.com>
2016-11-22 12:58:05 +0800

    modify

--- src/watcher/ICBCWatcher.py
+++ src/watcher/ICBCWatcher.py
@@ -9,24 +9,24 @@ class ICBCWatcher(PageWatcher):
         return 'http://www.icbc.com.cn/FinanceMarket/gold/Default.aspx'
 
     def process(self, document):
-        strPrice1 = document("#ctl00_Content_Boby_AccountGold1_Label_mrj1")
-        price1 = stringUtils.getAmount(strPrice1.text())
+        str_price1 = document("#ctl00_Content_Boby_AccountGold1_Label_mrj1")
+        price1 = stringUtils.getAmount(str_price1.text())
         # print('银行买入价：%s元/克' % price1)
 
-        strPrice2 = document('#ctl00_Content_Boby_AccountGold1_Label_mcj1')
-        price2 = stringUtils.getAmount(strPrice2.text())
+        str_price2 = document('#ctl00_Content_Boby_AccountGold1_Label_mcj1')
+        price2 = stringUtils.getAmount(str_price2.text())
         # print('银行卖出价：%s元/克' % price2)
 
-        strPrice3 = document('#ctl00_Content_Boby_AccountGold1_Label_zjj1')
-        price3 = stringUtils.getAmount(strPrice3.text())
+        str_price3 = document('#ctl00_Content_Boby_AccountGold1_Label_zjj1')
+        price3 = stringUtils.getAmount(str_price3.text())
         # print('中间价：%s元/克' % price3)
 
         # 到达设定值时，发送警告
         highest = float(280)
         lowest = float(265)
-        if price1 > highest:
+        if (price1 is not None) and (price1 > highest):
             self.notify('银行买入价高于设定值%s，目前价格%s' % (highest, price1))
-        if price1 < lowest:
+        if (price1 is not None) and (price1 < lowest):
             self.notify('！！！银行买入价低于设定值%s，目前价格%s' % (lowest, price1))
         pass
 
