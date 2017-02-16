4d9136c12d32c5f50077c41327b166e43c09e2e7
sweet <sweet123951@gmail.com>
2017-02-16 19:49:37 +0800

    fix: pushNotify.py config code

--- src/notify/PushNotify.py
+++ src/notify/PushNotify.py
@@ -9,15 +9,15 @@ __author__ = 'sweet'
 
 class PushNotify(object):
     def __init__(self):
-        self.config = Config.config
-        self.token = self.config.get_dict()['pushbullet']['token']
+        self.config = Config.Config.config
+        self.token = lambda: self.config.get_dict()['pushbullet']['token']
 
     def push_msg(self, title, content):
         url = 'https://api.pushbullet.com/v2/pushes'
         req = {'type': 'note', 'title': title, 'body': content}
         data = json.dumps(req)
         logging.debug('data: ' + data)
-        response = requests.post(url, data, headers={'Content-Type': 'application/json', 'Access-Token': self.token})
+        response = requests.post(url, data, headers={'Content-Type': 'application/json', 'Access-Token': self.token()})
         result = json.loads(response.text)
         if 'iden' in result:
             return True
@@ -26,5 +26,5 @@ class PushNotify(object):
 
 if __name__ == "__main__":
     push = PushNotify()
-    print(push.token)
-    push.push_msg('test', u'测试下')
+    print(push.token())
+    # push.push_msg('test', u'测试下')
